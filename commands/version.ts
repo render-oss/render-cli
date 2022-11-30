19e39fc444291c62f4dd9caa5c8bf81578fe448e
Ed Ropple <ed@render.com>
2022-11-30 16:37:37 -0500

    added more machine-readable 'version' command

---
+++ commands/version.ts
@@ -0,0 +1,12 @@
+import { VERSION } from "../version.ts";
+import { Subcommand } from "./_helpers.ts";
+
+const desc = `Shows the application version.`;
+
+export const versionCommand =
+  new Subcommand()
+    .name('version')
+    .description(desc)
+    .action(() => {
+      console.log(`v${VERSION}`);
+    });
