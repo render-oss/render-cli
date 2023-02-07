3c1a5d387528178ea0a032ef65e2d7a71a48c38e
Ed Ropple <ed@render.com>
2023-02-07 15:27:44 -0500

    verbiage fix

--- new/repo/index.ts
+++ new/repo/index.ts
@@ -99,10 +99,8 @@ export async function templateNewProject(args: TemplateNewProjectArgs): Promise<
     console.log("");
     console.log("and you're good to go!");
     console.log("");
-    console.log(Cliffy.colors.brightWhite("Now that you have a new project set up, you'll need to push to GitHub or GitLab"))
-    console.log(`${Cliffy.colors.brightWhite("in order to deploy it to Render")}. Once you've followed the instructions`);
-    console.log(`provided when you create your repo in GitHub or GitLab, you can then run`);
-    console.log(`${Cliffy.colors.cyan("render buildpack launch")} to easily deploy your project to Render.`);
+    console.log(Cliffy.colors.brightWhite("Now that you have a new blueprint set up, you'll need to push it to your repo."))
+    console.log(`Once done, ${Cliffy.colors.cyan("render buildpack launch")} to easily deploy your project to Render.`);
     console.log("");
     console.log("Thanks for using Render, and good luck with your new project!");
   } finally {
@@ -225,10 +223,8 @@ export async function writeExampleBlueprint(
     console.log("");
     console.log(Cliffy.colors.brightWhite("Please make sure to review the blueprint's contents to make sure they're appropriate for your app!"));
     console.log("");
-    console.log(Cliffy.colors.brightWhite("Now that you have a new project set up, you'll need to push to GitHub or GitLab"))
-    console.log(`${Cliffy.colors.brightWhite("in order to deploy it to Render")}. Once you've followed the instructions`);
-    console.log(`provided when you create your repo in GitHub or GitLab, you can then run`);
-    console.log(`${Cliffy.colors.cyan("render buildpack launch")} to easily deploy your project to Render.`);
+    console.log(Cliffy.colors.brightWhite("Now that you have a new blueprint set up, you'll need to push it to your repo."))
+    console.log(`Once done, ${Cliffy.colors.cyan("render buildpack launch")} to easily deploy your project to Render.`);
     console.log("");
     console.log("Thanks for using Render, and good luck with your new project!");
   } finally {
