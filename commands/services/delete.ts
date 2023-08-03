b74f72c304f7c4f16d2c45eabd0ccc32220a1043
Bill Mill <bill@billmill.org>
2023-08-03 10:28:17 -0400

    add a delete command

---
+++ commands/services/delete.ts
@@ -0,0 +1,25 @@
+import { apiGetAction, Subcommand } from "../_helpers.ts";
+import { getConfig } from "../../config/index.ts";
+import { deleteRequestRaw } from "../../api/index.ts";
+import { getLogger } from "../../util/logging.ts";
+
+const desc = `Deletes a service`;
+
+export const servicesDeleteCommand = new Subcommand()
+  .name("list")
+  .description(desc)
+  .group("API parameters")
+  .option("--id <serviceId:string>", "the service ID (e.g. `srv-12345`)")
+  .action((opts) =>
+    apiGetAction({
+      processing: async () => {
+        const cfg = await getConfig();
+        const logger = await getLogger();
+
+        const ret = await deleteRequestRaw(logger, cfg, `/services/${opts.id}`);
+        logger.debug(`deleted service ${opts.id}`);
+
+        return ret;
+      },
+    })
+  );
