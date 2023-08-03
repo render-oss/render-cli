8fe3d790a17b56baa4a96287d259e6d0a8d619ef
Bill Mill <bill@billmill.org>
2023-08-03 11:03:20 -0400

    fix output and exit code for delete action

--- commands/services/delete.ts
+++ commands/services/delete.ts
@@ -1,4 +1,4 @@
-import { apiGetAction, Subcommand } from "../_helpers.ts";
+import { standardAction, Subcommand } from "../_helpers.ts";
 import { getConfig } from "../../config/index.ts";
 import { deleteRequestRaw } from "../../api/index.ts";
 import { getLogger } from "../../util/logging.ts";
@@ -11,13 +11,15 @@ export const servicesDeleteCommand = new Subcommand()
   .group("API parameters")
   .option("--id <serviceId:string>", "the service ID (e.g. `srv-12345`)")
   .action((opts) =>
-    apiGetAction({
+    standardAction({
+      exitCode: (res) => (res?.status == 204 ? 0 : 1),
+      interactive: () => undefined,
       processing: async () => {
         const cfg = await getConfig();
         const logger = await getLogger();
 
         const ret = await deleteRequestRaw(logger, cfg, `/services/${opts.id}`);
-        logger.debug(`deleted service ${opts.id}`);
+        logger.debug(`deleted service ${opts.id}: ${ret.status}`);
 
         return ret;
       },
